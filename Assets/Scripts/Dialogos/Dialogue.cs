f529014bc81d50b5aba9c3e7aa72009433dbaed8
Carlos Ivan <fatedarkness0@gmail.com>
2025-10-22 21:03:38 -0500

    Add Vorr animation controller, UI prefabs, and sprites
    Added Vorr 1 animation controller and related meta files, new Canvas and NPC prefabs, dialogue script, and multiple sprites for Vorr and warning signs. These assets support new character animations, UI elements, and visual feedback in the game.

---
+++ Assets/Scripts/Dialogos/Dialogue.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using TMPro;
+using System;
+using System.Collections;
+
+public class Dialogue : MonoBehaviour
+{
+
+    [SerializeField] private GameObject dialogueMark;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField, TextArea(4,6)] private string[] dialogueLines;
+    private int lineIndex;
+
+    private float typingTime = 0.05f;
+
+    private bool isPlayerInRange;
+    private bool didDialogueStart;
+
+    private void Update()
+    {
+        if (isPlayerInRange && Input.GetKeyDown("e"))
+        {
+            if (!didDialogueStart)
+            {
+                StartDialogue();
+            }
+            else if (dialogueText.text == dialogueLines[lineIndex])
+            {
+                NextDialogueLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                dialogueText.text = dialogueLines[lineIndex];
+            }
+        }
+    }
+
+    private void StartDialogue()
+    {
+        didDialogueStart = true;
+        dialoguePanel.SetActive(true); 
+        dialogueMark.SetActive(false);
+        lineIndex = 0;
+        Time.timeScale = 0f; // Pausa el juego
+        StartCoroutine(ShowLine());
+    }
+
+    private void NextDialogueLine()
+    {
+        lineIndex++;
+        if(lineIndex < dialogueLines.Length)
+        {
+            StartCoroutine(ShowLine());
+        }
+        else
+        {
+            dialoguePanel.SetActive(false);
+            didDialogueStart = false;
+            dialogueMark.SetActive(true);
+            Time.timeScale = 1f; // Reanuda el juego
+        }
+    }
+
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+        foreach(char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSecondsRealtime(typingTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+            dialogueMark.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            dialogueMark.SetActive(false);
+        }
+    }
+}
